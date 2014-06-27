0d3e1aa1d45b5451c66baa60f95908d26a997ce3
Akdemia <Juan@The-iMac.local>
2014-06-27 15:02:24 -0430

    Making Signup page (antes de que se vaya la luz)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-
   root 'static_pages#home'
 
   match '/help',    to: 'static_pages#help',    via: 'get'
@@ -9,6 +7,7 @@ Rails.application.routes.draw do
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/signup',  to: 'users#new',            via: 'get'
 
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
