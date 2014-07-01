e0d5c1db1d226804405250cdfdcf42fff1965c22
Akdemia <Juan@The-iMac.local>
2014-07-01 12:21:07 -0430

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,28 @@ class User < ActiveRecord::Base
 
     validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
     validates :name,  presence: true, length: { maximum: 50 },
-                      uniqueness: { case_sensitive: false }
+    uniqueness: { case_sensitive: false }
     validates :password, length: { minimum: 6 }
 
     has_secure_password
 
     before_save :set_downcaseEmail
+    before_create :create_remember_token
 
     def set_downcaseEmail
       self.email = email.downcase
     end
+
+    def User.new_remember_token
+        SecureRandom.urlsafe_base64
+    end
+
+    def User.digest(token)
+        Digest::SHA1.hexdigest(token.to_s)
+    end
+
+    private
+        def create_remember_token
+          self.remember_token = User.digest(User.new_remember_token)
+        end
 end
